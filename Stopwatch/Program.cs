0ed1102883aa9fe130c48837bfe27f083f04f496
jlswift2 <jlswift2@gmail.com>
2022-06-06 23:27:31 -0500

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            int milliseconds = 10000;
+            Thread.Sleep(milliseconds);
+            watch.Stop();
+
+            watch.Start();
+            Thread.Sleep(milliseconds);
+            watch.Stop();
+
+            var duration = watch.Duration;
+            Console.WriteLine($"The duration was {duration.Minutes} minutes and {duration.Seconds} seconds.");
+
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _start;
+        private DateTime _end;
+        private bool _isRunning;
+        private TimeSpan _interval;
+
+        public TimeSpan Duration
+        {
+            get { return _interval; }
+        }
+        public void Start()
+        {
+           if (_isRunning == false)
+            {
+                _start = DateTime.Now;
+                _isRunning = true;
+            }
+           else
+           {
+                throw new InvalidOperationException("The stopwatch is already running");
+           }
+        }
+
+        public void Stop()
+        {
+            if (_isRunning == true)
+            {
+                _isRunning = false;
+                _end = DateTime.Now;
+                _interval = _interval + (_end - _start);
+            } 
+        }
+    }
+}
